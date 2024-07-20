265e74c2f409da0451a64c3c7b4b3cd249684f6e
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-21 03:56:51 +0700

    Membuat View, Studi Kasus PHP OOP Aplikasi Todolist

---
+++ 5-membuat-view/view/TodolistView.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace View {
+
+    class TodolistView
+    {
+        function showTodolist(): void
+        {
+            # Kode Kosong
+        }
+
+        function addTodolist(): void
+        {
+            # Kode Kosong
+        }
+
+        function removeTodolist(): void
+        {
+            # Kode Kosong
+        }
+    }
+}
