3a8d42211a3523a88a32799873626d4d53d130af
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-21 04:15:36 +0700

    Repo & Service Menampilkan Todolist, Studi Kasus PHP OOP Aplikasi Todolist

---
+++ 6-repo-&-service-menampilkan-todolist/service/TodolistService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Service {
+
+    use Repository\TodolistRepository;
+
+    interface ToDoListService
+    {
+        function showToDoList(): void;
+        function addToDoList(string $todo): void;
+        function removeToDoList(int $number): void;
+    }
+
+    class ToDoListServiceImpl implements ToDoListService
+    {
+        private TodolistRepository $todolistRepository;
+
+        public function __construct(TodolistRepository $todolistRepository)
+        {
+            $this->todolistRepository = $todolistRepository;
+        }
+
+        public function showToDoList(): void
+        {
+            echo "TODOLIST" . PHP_EOL;
+            $todolist = $this->todolistRepository->findAll();
+            foreach ($todolist as $number => $value) {
+                echo "$number. $value" . PHP_EOL;
+            }
+        }
+
+        public function addToDoList(string $todo): void
+        {
+            # Kode Kosong.
+        }
+
+        public function removeToDoList(int $number): void
+        {
+            # Kode Kosong.
+        }
+    }
+}
